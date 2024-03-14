b9c1e1cdb9714206c88e92c6000d429e0bd64d3f
Kseniya <k.nemtyryova@gmail.com>
2024-03-14 10:46:25 +0300

    Добавьте файлы проекта.

---
+++ Classes/ClassStudent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Classes
+{
+    class ClassStudent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set;}
+
+        public int Age { get; set; }
+        public string Phone { get; set; }
+        public double Scholarship { get; set; }
+
+        public ClassStudent() 
+        {
+            Id= 0;
+            Name = string.Empty;
+            LastName = string.Empty;
+            Age = 0;
+            Phone = string.Empty;
+            Scholarship = 0;
+        }
+        public ClassStudent(int id, string lastname, string name, int age, string phone, double scholarship)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastname;
+            Age = age;
+            Phone = phone;
+            Scholarship = scholarship;
+        }
+    }
+}
---
+++ Pages/PageMain.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Classes;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageMain.xaml
+    /// </summary>
+    public partial class PageMain : Page
+    {
+        public PageMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDeataGridOpen_Click(object sender, RoutedEventArgs e)
+        {
+            ClassHelper.frame.Navigate(new PageDataGrid());
+        }
+    }
+}
